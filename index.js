fc5886a5a771037d4dfed6b48284f7afacec0de5
Tom Atkinson <tomatkinson93@gmail.com>
2026-05-16 23:15:11 -0400

    Remove stray routes/buildings.js, remove dead require from index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const authRoutes = require('./routes/auth');
 const gameRoutes = require('./routes/game');
 const mapRoutes = require('./routes/map');
 const citizenRoutes = require('./routes/citizens');
-const buildingRoutes = require('./routes/buildings');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -37,7 +36,6 @@ app.use('/api/auth', authRoutes);
 app.use('/api/game', gameRoutes);
 app.use('/api/map', mapRoutes);
 app.use('/api/citizens', citizenRoutes);
-app.use('/api/buildings', buildingRoutes);
 const expeditionRoutes = require('./routes/expeditions');
 app.use('/api/expeditions', expeditionRoutes);
 const questRoutes = require('./routes/quests');
